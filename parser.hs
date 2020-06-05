74cda01bcf1036f9ea208526d004c9cbfa036c41
Christoph Rueff <christoph.rueff@gmail.com>
2020-06-05 13:36:04 +0200

    Add Parser module with term parser

---
+++ parser.hs
@@ -0,0 +1,35 @@
+module Parser where
+
+import Control.Applicative
+import Data.Char
+
+data Parser a = Parser (String -> [(a, String)])
+data Expression = Value Int | Operator Expression Char Expression | Group Char Expression Char deriving Show
+
+parse (Parser p) cs = p cs
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser (\cs -> [(f x, ys) | (x, ys) <- p cs])
+
+instance Applicative Parser where
+  pure x = Parser (\cs -> [(x, cs)])
+  p <*> q = Parser (\cs -> [y | (x, ys) <- parse p cs, y <- parse (fmap x q) ys])
+
+instance Alternative Parser where
+  empty = Parser (\_ -> [])
+  p <|> q = Parser (\cs -> case parse p cs of
+    [] -> parse q cs
+    ps -> ps)
+
+split _ [] = []
+split p (c:cs) = if p c then [(c, cs)] else []
+
+char c = Parser (split (==c))
+digit = Parser (split isDigit)
+
+term = Operator <$> facteur <*> char '+' <*> term <|> facteur
+facteur = Operator <$> expression <*> char '*' <*> facteur <|> expression
+expression = entier <|> group
+entier = (Value . read) <$> some digit
+group = Group <$> char '(' <*> term <*> char ')'
+
